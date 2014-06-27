cf602d0923887528df10d5c38c6e8d0c441587ce
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-06-26 20:19:16 -0700

    first commit

---
+++ Superpowers/VWWAssetCollectionViewCell.m
@@ -0,0 +1,22 @@
+//
+//  VWWAssetCollectionViewCell.m
+//  Superpowers
+//
+//  Created by Zakk Hoyt on 6/26/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import "VWWAssetCollectionViewCell.h"
+
+@implementation VWWAssetCollectionViewCell
+
+- (instancetype)initWithFrame:(CGRect)frameRect
+{
+    self = [super initWithFrame:frameRect];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
